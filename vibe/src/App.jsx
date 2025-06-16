77ded0038956ef90d8055a370f995e91bb845389
pen-code <sourav6102003@gmail.com>
2025-06-16 17:12:02 +0530

    initial commit

---
+++ vibe/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+import Dasboard from './components/Dasboard.jsx'
+
+function App() {
+  return (
+    <>
+      <Dasboard />
+    </>
+
+  )
+}
+
+export default App;
